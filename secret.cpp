d4df580963a173b9c17bb726577e4150484c8b14
Timothy B <b85205@gmail.com>
2017-07-11 16:43:32 -0400

    Re implemented Caesar/Vigenere encrypt. Added method for shifting letters and a method to check for upper/lower case letters.

--- secret.cpp
+++ secret.cpp
@@ -7,91 +7,158 @@
 //----------------------------------------------------
 
 void Caesar::encrypt(std::string key) {
-	 	 int key_len = key.length();
-	 	 int msg_len = message.length();
-	 	 int ii=0;
-	 	 // unsigned int c, ii=0, newchar;
- 
-  
+    //NOTE: it only makes sense to have a key as a number when implementing a caesar cipher
+    int numKey = std::stoi(key);//If this is not a number it will throw fail...
+    int msg_len = (int) message.length();
 
-	 	 for(int i=0; i<msg_len; i++){
-	 	 	message[i] += key[ii];
-	 	 	ii = (ii + 1) % key_len;
-	 	 }
+    for (int i = 0; i < msg_len; i++) {
+        message[i] = shifter(message[i], numKey);
+    }
 
-
-	} 
+    isEncrypted = true;
+}
 
 void Caesar::decrypt(std::string key) {
-	 	 int key_len = key.length();
-	 	 int msg_len = message.length();
-	 	 int ii=0;
-	 	 // unsigned int c, ii=0, newchar;
- 
-  
+    int key_len = (int) key.length();
+    int msg_len = (int) message.length();
+    int ii = 0;
+    // unsigned int c, ii=0, newchar;
 
-	 	 for(int i=0; i<msg_len; i++){
-	 	 	message[i] -= key[i];
-	 	 	ii = (ii + 1) % key_len;
-	 	 }
-	} 
 
-	
+
+    for (int i = 0; i < msg_len; i++) {
+        message[i] -= key[i];
+        ii = (ii + 1) % key_len;
+    }
+}
+
+
 //----------------------------------------------------
 
 void Vigenere::encrypt(std::string key) {
-		 std::string s = key;
-	 	 int key_len = s.length();
-	 	 unsigned int c, ii=0, newchar;
- 
-  	while ((c = getchar()) != EOF) {
-	    newchar = c + s[ii];
-	    s[ii] = c;   
-		std::cout << newchar; 
-  		ii = (ii + 1) % key_len;
-  	 }
+    int keyLength = (int) key.length();
+    int msg_len = (int) message.length();
+    int count = 0;
 
-	} // End of Caesar Encrypt
+    for (int i = 0; i < msg_len; i++) {
+        message[i] = shifter(message[i], key[count]);
+        count = count < keyLength ? count + 1 : 0;
+    }
+
+    isEncrypted = true;
+
+} // End of Caesar Encrypt
 
 void Vigenere::decrypt(std::string key) {
-		std::string s = key;
-	 	 int key_len = s.length();
-	 	 unsigned int c, ii=0, newchar;
- 
-  	while ((c = getchar()) != EOF) {
-	    newchar = c - s[ii];
-	    s[ii] = c;   
-		std::cout << newchar; 
-  		ii = (ii + 1) % key_len;
-  	 }
-	}
+    std::string s = key;
+    int key_len = (int) s.length();
+    unsigned int c, ii = 0, newchar;
+
+    while ((c = (unsigned int) getchar()) != EOF) {
+        newchar = c - s[ii];
+        s[ii] = (char) c;
+        std::cout << newchar;
+        ii = (ii + 1) % key_len;
+    }
+}
 //----------------------------------------------------
 
 void Autokey::encrypt(std::string key) {
-	int key_len = key.length();
-	 	 int msg_len = message.length();
-	 	 int ii=0;
-	 	 // unsigned int c, ii=0, newchar;
- 
-  
+    int key_len = (int) key.length();
+    int msg_len = (int) message.length();
+    int ii = 0;
+    // unsigned int c, ii=0, newchar;
 
-	 	 for(int i=0; i<msg_len; i++){
-	 	 	message[i] += key[ii];
-	 	 	ii = (ii + 1) % key_len;
-	 	 }
 
-	} 
+
+    for (int i = 0; i < msg_len; i++) {
+        message[i] += key[ii];
+        ii = (ii + 1) % key_len;
+    }
+
+}
 
 void Autokey::decrypt(std::string key) {
-	int key_len = key.length();
-	 	 int msg_len = message.length();
-	 	 int ii=0;
-	 	 // unsigned int c, ii=0, newchar;
- 
-  
+    int key_len = (int) key.length();
+    int msg_len = (int) message.length();
+    int ii = 0;
+    // unsigned int c, ii=0, newchar;
 
-	 	 for(int i=0; i<msg_len; i++){
-	 	 	message[i] += key[ii];
-	 	 	ii = (ii + 1) % key_len;
-	 	 }
-	}
+
+
+    for (int i = 0; i < msg_len; i++) {
+        message[i] += key[ii];
+        ii = (ii + 1) % key_len;
+    }
+}
+
+/**
+ * @param letter this is the letter to be shifted.
+ * @param shift this is the count of how many places over to shift letter.
+ * @return the char in its shifted form. i.e. if letter 'a' is given with a shift of 3 then the result will be letter 'd'
+ */
+char Secret::shifter(char letter, int shift) {
+    if(letter == ' ' || letter < 32 || letter > 126){
+        return letter;
+    }
+    const int startOfUpperCase = 64;
+    const int startOfLowerCase = 96;
+    const int totalPossibleLetters = 26;
+
+    const int startOfAllDec = 32;
+    const int totalPossibleValues = 126;
+    if (isUpperCase(letter)) {
+        return (char) ((((letter + shift) - startOfUpperCase) % totalPossibleLetters) + startOfUpperCase);
+    } else if (isLowerCase(letter)) {
+        return (char) ((((letter + shift) - startOfLowerCase) % totalPossibleLetters) + startOfLowerCase);
+    }
+
+    return (char) ((((letter + shift) - startOfAllDec) % totalPossibleValues) + startOfAllDec);
+}
+
+/**
+ * @param letter this is the letter to be shifted.
+ * @param shift this is the count of how many places over to shift letter.
+ * @return the char in its shifted form. i.e. if letter 'a' is given with a shift of 3 then the result will be letter 'd'
+ */
+char Secret::shifter(char letter, char shift) {
+    if(letter == ' ' || letter < 32 || letter > 126){
+        return letter;
+    }
+    const int startOfUpperCase = 64;
+    const int startOfLowerCase = 96;
+    const int totalPossibleLetters = 26;
+
+    const int startOfAllDec = 32;
+    const int totalPossibleValues = 126;
+    if (isUpperCase(letter)) {
+        shift = (char) toupper(shift);
+        shift = (char) (shift - startOfUpperCase);
+
+        return (char) ((((letter + shift) - startOfUpperCase) % totalPossibleLetters) + startOfUpperCase);
+    } else if (isLowerCase(letter)) {
+        shift = (char) tolower(shift);
+        shift = (char) (shift - startOfLowerCase);
+
+        return (char) ((((letter + shift) - startOfLowerCase) % totalPossibleLetters) + startOfLowerCase);
+    }
+
+    shift = (char) (shift - startOfAllDec);
+    return (char) ((((letter + shift) - startOfAllDec) % totalPossibleValues) + startOfAllDec);
+}
+
+/*
+ * Check to see if a char is upper case or not
+ * Returns a bool
+ */
+bool Secret::isUpperCase(char letter) {
+    return (letter >= 'A') && (letter <= 'Z');
+}
+
+/*
+ * Check to see if a char is upper case or not
+ * Returns a bool
+ */
+bool Secret::isLowerCase(char letter) {
+    return (letter >= 'a') && (letter <= 'z');
+}
